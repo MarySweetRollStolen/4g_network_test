e77822747056ea9f20462fd423ab8ce3afe410a2
MarySweetRollStolen <marylostherself.com>
2022-12-22 19:32:22 +0200

    Add img generator

--- app.js
+++ app.js
@@ -5,3 +5,37 @@ document.getElementById('buttonPopUpOpen').addEventListener('click', () => {
 document.getElementById('buttonPopUpClose').addEventListener('click', () => {
     document.querySelector('.popup-back').style.display = "none";
 })
+
+function generateImg(source) {
+    const img = document.createElement('img');
+    img.src = source;
+    img.style.width = '60px';
+    img.style.height = '60px';
+    img.style.margin = '5px';
+    return img;
+}
+
+function generateImgArray() {
+    const imgSources = [
+        'BSC.jpg',
+        'bts.jpg',
+        'EIR.png',
+        'enodeb.jpg',
+        'mme.jpg',
+        'pcrf.jpg',
+        'hss.png',
+        'pdngate.png',
+        'servgate.png'
+    ]
+    let imgArray = [];
+    for (let i = 0; i < imgSources.length; i++) {
+        imgArray.push(generateImg('images/components/' + imgSources[i]));
+    }
+    return imgArray;
+}
+
+imgArray = generateImgArray();
+imgArray.forEach(img => {
+document.getElementById('component').appendChild(img);
+});
+

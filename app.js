b9b29cc604dfcd473d8829c6be43a0a2e7f03405
MarySweetRollStolen <marylostherself.com>
2022-12-22 17:27:17 +0200

    init add greetin block

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('buttonPopUpOpen').addEventListener('click', () => {
+    document.querySelector('.popup-back').style.display = "flex";
+})
+
+document.getElementById('buttonPopUpClose').addEventListener('click', () => {
+    document.querySelector('.popup-back').style.display = "none";
+})

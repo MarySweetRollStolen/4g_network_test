9cdd4c3a12b0ecd2fafeb229ea7a117c14baa422
MarySweetRollStolen <marylostherself.com>
2022-12-24 06:16:37 +0200

    Add schema

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-document.getElementById('buttonPopUpOpen').addEventListener('click', () => {
-    document.querySelector('.popup-back').style.display = "flex";
-})
-
-document.getElementById('buttonPopUpClose').addEventListener('click', () => {
-    document.querySelector('.popup-back').style.display = "none";
-})
-
-function generateImg(source) {
-    const img = document.createElement('img');
-    img.src = source;
-    img.style.width = '60px';
-    img.style.height = '60px';
-    img.style.margin = '5px';
-    return img;
-}
-
-function generateImgArray() {
-    const imgSources = [
-        'BSC.jpg',
-        'bts.jpg',
-        'EIR.png',
-        'enodeb.jpg',
-        'mme.jpg',
-        'pcrf.jpg',
-        'hss.png',
-        'pdngate.png',
-        'servgate.png'
-    ]
-    let imgArray = [];
-    for (let i = 0; i < imgSources.length; i++) {
-        imgArray.push(generateImg('images/components/' + imgSources[i]));
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonPopUpOpen').addEventListener('click', () => {
+        document.querySelector('.popup-back').style.display = "flex";
+    })
+    
+    document.getElementById('buttonPopUpClose').addEventListener('click', () => {
+        document.querySelector('.popup-back').style.display = "none";
+    })
+    
+    function generateImg(source) {
+        const img = document.createElement('img');
+        img.src = 'images/components/'+source;
+        img.style.width = '60px';
+        img.style.height = '60px';
+        img.style.margin = '5px';
+        img.id = source.substring(0, source.indexOf('.'));
+        img.draggable = true;
+        img.classList.add('dragable');
+        return img;
     }
-    return imgArray;
-}
-
-imgArray = generateImgArray();
-imgArray.forEach(img => {
-document.getElementById('component').appendChild(img);
-});
-
+    
+    function generateImgArray() {
+        const imgSources = [
+            'BSC.jpg',
+            'bts.jpg',
+            'EIR.png',
+            'enodeb.jpg',
+            'mme.jpg',
+            'pcrf.jpg',
+            'hss.png',
+            'pdngate.png',
+            'servgate.png'
+        ]
+        let imgArray = [];
+        for (let i = 0; i < imgSources.length; i++) {
+            imgArray.push(generateImg(imgSources[i]));
+        }
+        return imgArray;
+    }
+    
+    imgArray = generateImgArray();
+    imgArray.forEach(img => {
+    document.getElementById('component').appendChild(img);
+    });
+    
+    
+})
